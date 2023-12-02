8d5105b440e7f27e3f25580d88b435565c2fb871
Krishna <tamadakrishnaa@gmail.com>
2023-12-02 12:29:01 +0530

    deploy

--- index.js
+++ index.js
@@ -1,61 +1,18 @@
-const cors = require('cors');
 const express = require('express');
-const app = express();
 const path = require('path');
-require('dotenv').config({ path : "./config.env"});
-const port = process.env.PORT || 5000;
 
-app.use(express.json());
+const app = express();
 
-// use middleware
-app.use(cors({
-    origin:'*',
-    methods: ['GET','POST','DELETE']
-}));
+// Serve static files from the 'build' folder
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-    //middleware
-    app.use(cors({
-        origin:'*',
-        methods: ['GET','POST','DELETE']
-    }));
+// Handle other routes and serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
-    app.use(function (req, res, next){
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header(
-            "Access-Control-Allow-Methods",
-            "GET, HEAD, OPTIONS, POST, PUT, DELETE"
-        );
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-        next();
-    });
-    
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-
-    app.use(cors({
-        origin:'*',
-        methods: ['GET','POST','DELETE']
-    }));
-
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-
-
-    app.use(function (req, res, next){
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header(
-            "Access-Control-Allow-Methods",
-            "GET, HEAD, OPTIONS, POST, PUT, DELETE"
-        );
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-        next();
-    });
-
-}
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
