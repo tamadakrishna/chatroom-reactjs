4c74a60332ad4e27e57d89835c30c53bf35fc157
tamadakrishna <tamadakrishnaa@gmail.com>
2024-02-18 22:40:29 +0530

    client

--- src/PrivateChat.jsx
+++ src/PrivateChat.jsx
@@ -110,7 +110,10 @@ function PrivateChat() {
             <div className="w-[150px] h-[100%] flex justify-center items-center p-1 ">
                 <button type="button" className=" h-[100%] w-[100%] py-2.5 px-5  text-sm  font-medium text-gray-900 focus:outline-none bg-white rounded-[10px] border border-gray-400 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 
                                         dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                        onClick={()=>{}}>
+                        onClick={(e)=>{
+                            setMessageInput('');
+                            OnEmit(e)
+                        }}>
                     Send
                 </button>
             </div>
